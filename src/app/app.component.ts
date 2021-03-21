4e151800d846e8a3deb7db7de3efad924786e9fe
Eray Bakır <info@eraybakir.com>
2021-03-21 22:49:00 +0300

    Skip Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { of, from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { skip } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,13 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
+    var numberArray = from([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+    //skip metoduyla 3 adet datayı atlamış olduk
+    numberArray.pipe(skip(3)).subscribe(data => {
+      console.log(data);
+    });
+
 
   }
 
