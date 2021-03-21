eb692ef1f980880a6c58564896201e9c74983e40
Eray Bakır <info@eraybakir.com>
2021-03-21 19:51:06 +0300

    Pipe and First Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { of, from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { first } from "rxjs/operators" //first işlevini kullanabilmek için import ettik
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,12 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
+
+    const myArray = from([5, 10, 15, 20, 50, 100, 600]);
+    //pipe bir dizi observable eylemini ardışık olarak uygulayabilen bir fonksiyondur.First ise LINQ da ki firstordefault() gibi düşünülebilir
+    myArray.pipe(first(val=>val===15)).subscribe(data => {
+      console.log(data);
+    });
 
   }
 
