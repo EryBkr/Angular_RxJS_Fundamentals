c3dd0c8e69923cf857c4e39c319a70327ab860c1
Eray Bakır <info@eraybakir.com>
2021-03-21 23:44:37 +0300

    Map Exampler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { of, from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,12 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
+    const myArray = from([1, 2, 3, 4, 5, 6, 7, 8]);
 
+    //Map her bir değere erişip onu manipüle edebilmemizi sağlar
+    myArray.pipe(map(value=>value+100)).subscribe(data=>{
+      console.log(data);
+    });
   }
 
 }
