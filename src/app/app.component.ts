855882ac26fb071b6ffbef5cd9c2f82a0d3b5287
Eray Bakır <info@eraybakir.com>
2021-03-22 00:36:27 +0300

    Reduce Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of, from, interval } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
-import { map, skip, take, toArray } from "rxjs/operators";
+import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { map, reduce } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -12,10 +12,13 @@ export class AppComponent {
 
   constructor() {
 
-    //Oluşan dataları dizi içerisinde sunar
-    interval(100).pipe(skip(10), take(5), toArray()).subscribe(data => {
-      console.log(data);
-    });
+   const myArray=of(1,2,3,5,6,7,8);
+
+
+   //Sum burada örnektir.Farklı işlemler için reduce kullanabiliriz.Temp değişken mantığı vardır
+   myArray.pipe(reduce((sum,val)=>sum+val)).subscribe(data=>{
+     console.log(data);
+   });
 
   }
 
