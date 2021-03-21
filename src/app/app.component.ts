03bc69f13c649356a0322e268a39ef8bd90166b3
Eray Bakır <info@eraybakir.com>
2021-03-21 15:33:12 +0300

    Ajax example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { ajax } from "rxjs/ajax"; //Ajax için farklı bir import yolu seçmemiz gerekiyor
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,16 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
-
+    //GET isteği yapıyoruz.Zaten bildiğimiz ajax yapısı bu.
+    const getData = ajax.getJSON("https://jsonplaceholder.typicode.com/posts");
+    getData.subscribe(data => {
+      //Ajax isteği atarak datalarımıza ulaşmış olduk
+      console.table(data); //Table datayı daha düzenli görmemizi sağlayan bir js fonksiyonudur
+    },
+      err => { },
+      () => {
+        console.log("İşlem tamamdır");
+      });
   }
 
 }
