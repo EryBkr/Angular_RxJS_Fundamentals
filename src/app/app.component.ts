fb7f61c40c79d44b8da162d23b033b6442f9c5ca
Eray Bakır <info@eraybakir.com>
2021-03-21 16:59:47 +0300

    From Event Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
-import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
+
+  //Html tarafında ki buttonumuza bu şekilde decarator aracılığıyla ulaşıyorum
+  @ViewChild("myBtn") //Parametre olarak #myBtn referansını veriyorum
+  myButton: ElementRef;
+
+
 
   constructor() {
+  }
 
+  //Component oluşurken çalışacak olan kısım
+  ngOnInit(): void {
+  }
+
+  //View elemanları oluştuktan sonra çalışacak kısım
+  ngAfterViewInit(): void {
+    //ilk parametreye buttonumuzun native halini,ikinci parametreye ise event tipini verdik.Bu yapıya subscribe olup gerekli işlemleri gerçekleştireceğiz
+    fromEvent(this.myButton.nativeElement, "click").subscribe(execute => {
+      alert("Çalıştı");
+    });
   }
 
 }
