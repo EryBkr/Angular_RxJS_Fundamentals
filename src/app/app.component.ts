b7f1538caca36d7f1b8b4fd94e27de7b0512f67c
Eray Bakır <info@eraybakir.com>
2021-03-21 23:14:00 +0300

    Throttle Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { interval, of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import {throttle} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,12 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
+    const myInterval = interval(1000);
 
+    //Gecikmeli olarak data yayınlamamızı sağlar
+    myInterval.pipe(throttle(x=>interval(2000))).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
