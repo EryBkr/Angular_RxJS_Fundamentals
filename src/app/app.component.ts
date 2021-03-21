e37458b954ccf71c7a51bd181fab14b01fa4c3c6
Eray Bakır <info@eraybakir.com>
2021-03-21 22:56:22 +0300

    SkipUntil Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { of, from, interval, timer } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { skipUntil } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,12 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
+    const myInterval = interval(1000);
 
+    //Zaman düzeyinde atlama sağlıyor skipUntil
+    myInterval.pipe(skipUntil(timer(5000))).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
