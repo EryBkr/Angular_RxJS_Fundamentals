7ceb5d725320574b4a6d55f9071bff8f54e0b35a
Eray Bakır <info@eraybakir.com>
2021-03-22 00:31:27 +0300

    ToArray Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of, from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
-import { concatMap, delay, map } from "rxjs/operators";
+import { of, from, interval } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { map, skip, take, toArray } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -11,13 +11,12 @@ import { concatMap, delay, map } from "rxjs/operators";
 export class AppComponent {
 
   constructor() {
-    const myArray = from([1, 2, 3, 4, 5, 6, 7, 8]);
-    const otherOf = of("a", "b", "c", "d");
 
-    //iki farklı observable yapının datalarını birleştirmemizi sağlıyor.İlk gelen data grubunu yayınlıyor
-    otherOf.pipe(concatMap(value => myArray.pipe(delay(1000), map(number => value + number)))).subscribe(data=>{
+    //Oluşan dataları dizi içerisinde sunar
+    interval(100).pipe(skip(10), take(5), toArray()).subscribe(data => {
       console.log(data);
     });
+
   }
 
 }
