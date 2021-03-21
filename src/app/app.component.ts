1e0a7eded1de0b5958e7c20ef51650aeadd44762
Eray Bakır <info@eraybakir.com>
2021-03-21 14:04:45 +0300

    ınterval example was created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { interval } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,20 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
-
+    //1 sn de bir çalışacak bir yapı oluşturduk.Aşağıda ki ornekte subscribe metodunun alabileceği parametleri ele aldık
+    //çalışması durumunda , hata almamız durumunda ve iletişimin tamamlandığı durumları ele aldık
+    //metodumuz 1 sn de bir çalışacağı için complete kısmı hiçbir şekilde çalışmayacaktır
+    const publisher = interval(1000);
+    publisher.subscribe(val => {
+      console.log("Eray " + val)
+    },
+      err => {
+        //Hata alırsak çalışacak
+        console.log(err);
+      },
+      () => {
+        console.log("İşlem tamamlandı");
+      });
   }
 
 }
