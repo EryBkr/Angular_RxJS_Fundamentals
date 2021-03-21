fd73cefe8e41fe3398aabb22797c2cb7a01fbf7d
Eray Bakır <info@eraybakir.com>
2021-03-21 19:56:18 +0300

    Find Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { find } from "rxjs/operators" //find metodunu kullana bilmek için ekledik
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,11 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
-
+    const myArray = from([1, 2, 3, 4, 5, 6]);
+    //3 ten buyuk datayı getirmesini söylüyorum, first LINQ daki first e find ise firstordefault a karşılık gelmektedir
+    myArray.pipe(find(val => val > 3)).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
