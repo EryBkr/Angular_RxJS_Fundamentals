e9c3191642ccff02d1a5d716b94ede0a226c39cb
Eray Bakır <info@eraybakir.com>
2021-03-21 15:25:01 +0300

    Defer Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { defer, of,timer } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,22 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
+    const publisher = of(new Date());//Observable bir tarih oluşturduk,oluşturulduğu anın tarihi alırız
+    const publisherDefer = defer(() => of(new Date())); //Bu yapıda ise çağırdığımız an ki tarihi alıyoruz
+    const myTimer=timer(10000); //aradaki farkı görmek için timer oluşturduk
+
+    myTimer.subscribe(ex=>{
+      publisher.subscribe(val=>{
+        console.log("of operators "+val);
+      });
+    });
+
+    myTimer.subscribe(ex=>{
+      publisherDefer.subscribe(val=>{
+        console.log("defer operators "+val);
+      });
+    });
+
 
   }
 
