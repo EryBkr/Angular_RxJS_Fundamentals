450646d60b6916fbad306b843050087e8620b39e
Eray Bakır <info@eraybakir.com>
2021-03-21 23:48:40 +0300

    MapTo Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 import { of, from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
-import {map} from "rxjs/operators";
+import {mapTo} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -13,8 +13,8 @@ export class AppComponent {
   constructor() {
     const myArray = from([1, 2, 3, 4, 5, 6, 7, 8]);
 
-    //Map her bir değere erişip onu manipüle edebilmemizi sağlar
-    myArray.pipe(map(value=>value+100)).subscribe(data=>{
+    //MapTo her bir değeri vermiş olduğumuz değer ile değiştirir ve onu data adeti kadar yazar
+    myArray.pipe(mapTo("sabit değer")).subscribe(data=>{
       console.log(data);
     });
   }
