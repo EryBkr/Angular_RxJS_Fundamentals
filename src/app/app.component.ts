f3de7a99abc8a8e4561cd97243c8ddb438081185
Eray Bakır <info@eraybakir.com>
2021-03-21 13:52:25 +0300

    Fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,7 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
-    //of operatorü içerisinde istediğimiz yapıda değişken tanımlayabiliriz.Bunların hepsini Observable olarak bize döndürür
-    const ofValues = of("eray", 4, [1, 2, 3]);
-    //Observable yapımızda ki datalara ulaşmak için subscribe olduk.
-    ofValues.subscribe(data=>{
-      //Bu şekilde her bir dataya tek tek ulaştık
-      console.log(data);
-    });
+
   }
 
 }
