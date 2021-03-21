b7a94a0b5d0228ba125a44e50ce372f2bce1ae31
Eray Bakır <info@eraybakir.com>
2021-03-22 00:22:26 +0300

    SwitchMap Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 import { of, from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
-import { mergeMap, delay, map } from "rxjs/operators";
+import { switchMap, delay, map } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -14,8 +14,8 @@ export class AppComponent {
     const myArray = from([1, 2, 3, 4, 5, 6, 7, 8]);
     const otherOf = of("a", "b", "c", "d");
 
-    //iki farklı observable yapının datalarını birleştirmemizi sağlıyor
-    otherOf.pipe(mergeMap(value => myArray.pipe(delay(5000), map(number => value + number)))).subscribe(data=>{
+    //iki farklı observable yapının datalarını birleştirmemizi sağlıyor.En son gelen data grubunu yayınlıyor
+    otherOf.pipe(switchMap(value => myArray.pipe(delay(2000), map(number => value + number)))).subscribe(data=>{
       console.log(data);
     });
   }
