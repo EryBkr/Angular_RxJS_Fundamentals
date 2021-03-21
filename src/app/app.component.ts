9928556761fbd00728693453302fbcb7889a8ab7
Eray Bakır <info@eraybakir.com>
2021-03-21 20:00:07 +0300

    Last Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 import { from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
-import { filter } from "rxjs/operators"; //filter için ekledik
+import { last } from "rxjs/operators"; //last için ekledik
 
 @Component({
   selector: 'app-root',
@@ -12,8 +12,8 @@ export class AppComponent {
 
   constructor() {
     const myArray = from([1, 2, 3, 4, 5, 6]);
-    //LINQ da ki where fonksiyonuna karşılık gelir
-    myArray.pipe(filter(i => i > 3)).subscribe(data => {
+    //3 ten büyük son elemanı getirdik
+    myArray.pipe(last(i => i > 3)).subscribe(data => {
       console.log(data);
     });
   }
