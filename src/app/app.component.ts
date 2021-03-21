26713c4b971ebc30c49cf9388cdd6c94fafd422c
Eray Bakır <info@eraybakir.com>
2021-03-22 00:09:09 +0300

    MergeMap Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 import { of, from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
-import {mapTo} from "rxjs/operators";
+import { mergeMap, delay, map } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -12,9 +12,10 @@ export class AppComponent {
 
   constructor() {
     const myArray = from([1, 2, 3, 4, 5, 6, 7, 8]);
+    const otherOf = of("a", "b", "c", "d");
 
-    //MapTo her bir değeri vermiş olduğumuz değer ile değiştirir ve onu data adeti kadar yazar
-    myArray.pipe(mapTo("sabit değer")).subscribe(data=>{
+    //iki farklı observable yapının datalarını birleştirmemizi sağlıyor
+    otherOf.pipe(mergeMap(value => myArray.pipe(delay(5000), map(number => value + number)))).subscribe(data=>{
       console.log(data);
     });
   }
