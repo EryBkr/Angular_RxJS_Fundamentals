abb7e77aae31113c9c85ce20e4838beaf8dac77d
Eray Bakır <info@eraybakir.com>
2021-03-21 23:01:10 +0300

    Take Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 import { of, from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
-import { skipWhile } from "rxjs/operators";
+import { take } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -12,8 +12,8 @@ export class AppComponent {
 
   constructor() {
     var myArray = from([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    //Belirli bir şarta kadar atlama sağlar
-    myArray.pipe(skipWhile((value, index) => value < 5)).subscribe(data=>{
+    //Kaç adet data nın gelmesi gerektiğini belirliyoruz
+    myArray.pipe(take(3)).subscribe(data=>{
       console.log(data);
     });
   }
