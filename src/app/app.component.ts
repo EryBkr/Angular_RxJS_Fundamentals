65e82893423af22d4a11cea347cc4f00c3b2b55c
Eray Bakır <info@eraybakir.com>
2021-03-22 13:10:20 +0300

    MergeAll Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of, fromEvent, interval, merge } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { of, fromEvent, interval } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { mergeAll, map } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,15 @@ export class AppComponent {
 
   constructor() {
     const myEvent = fromEvent(document, "click");
-    const myInterval = interval(1000);
 
-    //Merge farklı Observable yapılarını beraber yayımlayan yapıdır.
-    const myMerge = merge(myEvent, myInterval).subscribe(data => {
+
+    const myMerge = myEvent.pipe(map(data => interval(1000)));
+
+    //İki farklı observable yapıyı birleştirmemizi ve eş zamanlı data yayımını sağlar
+    myMerge.pipe(mergeAll()).subscribe(data => {
       console.log(data);
     });
+
   }
 
 }
