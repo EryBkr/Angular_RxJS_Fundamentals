fe06650b53e0f6afdd1f94335fc139977f8baf40
Eray Bakır <info@eraybakir.com>
2021-03-22 22:11:24 +0300

    ForkJoin Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { forkJoin, of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { ajax } from "rxjs/ajax";
+
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,15 @@ export class AppComponent {
 
   constructor() {
 
+    //Her iki istek bittiği zaman birlikte datayı yayınlar,Herhangi bir property e ulaşmak istersek diye any olarak cast ettik
+    forkJoin({
+
+      first: ajax.getJSON<any>("https://jsonplaceholder.typicode.com/albums"),
+      second: ajax.getJSON<any>("https://jsonplaceholder.typicode.com/posts")
+
+    }).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
