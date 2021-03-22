48120518a38a41fcba092356ce6121ba3997e65e
Eray Bakır <info@eraybakir.com>
2021-03-22 13:14:43 +0300

    StartWith Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { of, from } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { startWith } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,12 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
+    const myArray = from([1, 2, 3, 4, 5, 6]);
 
+    //Verimizin başlangıcına istediğimiz değeri eklemiş olduk
+    myArray.pipe(startWith(0)).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
