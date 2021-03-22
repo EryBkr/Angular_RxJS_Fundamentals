ef59123e959e0978d22e473f9b68a772d7b62645
Eray Bakır <info@eraybakir.com>
2021-03-22 23:34:34 +0300

    Finalize Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { interval, of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { finalize, take } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,10 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
-
+    //İşlem tamamlandıktan sonra Execute edilecek işlemi tanımlamamızı sağlar
+    interval(1000).pipe(take(5), finalize(() => console.log("İşlem Tamamlandı"))).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
