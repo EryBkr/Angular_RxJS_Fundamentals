6c767955a6e9603020b83e7b34225fc11f59129b
Eray Bakır <info@eraybakir.com>
2021-03-23 16:23:18 +0300

    Retry Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 import { interval, of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
-import {  timeout,catchError, delay } from "rxjs/operators";
+import {  timeout,catchError, delay,retry } from "rxjs/operators";
 import {ajax} from "rxjs/ajax";
 
 @Component({
@@ -14,8 +14,8 @@ export class AppComponent {
   constructor() {
 
 
-    //Bizim belirlediğimiz sürede data gelmez ise hata fırlatıyoruz ya da o hatayı yakalayıp konsola yazdırıyoruz
-    ajax.getJSON("https://jsonplaceholder.typicode.com/posts/1").pipe(delay(2000),timeout(1000),catchError(err=>of("Data Geç Geliyor"))).subscribe(data => {
+    //Hata durumunda bizim belirleyeceğimiz sayı kadar tekrar etmesini sağlar
+    ajax.getJSON("https://jsonplaceholder.typicode.com/postss/1").pipe(retry(3),catchError(err=>of("Data Geç Geliyor"))).subscribe(data => {
       console.log(data);
     });
 
