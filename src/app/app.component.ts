5d8b49e11be1756c6942d78c01c724a4bc9c8ffe
Eray Bakır <info@eraybakir.com>
2021-03-23 16:33:50 +0300

    RetryWhen Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 import { interval, of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
-import {  timeout,catchError, delay,retry } from "rxjs/operators";
-import {ajax} from "rxjs/ajax";
+import { timeout, catchError, delay, retryWhen, map, tap } from "rxjs/operators";
+import { ajax } from "rxjs/ajax";
 
 @Component({
   selector: 'app-root',
@@ -13,11 +13,21 @@ export class AppComponent {
 
   constructor() {
 
+    const myInterval = interval(1000);
+    myInterval.pipe(map(value => {
 
-    //Hata durumunda bizim belirleyeceğimiz sayı kadar tekrar etmesini sağlar
-    ajax.getJSON("https://jsonplaceholder.typicode.com/postss/1").pipe(retry(3),catchError(err=>of("Data Geç Geliyor"))).subscribe(data => {
+      if (value > 6) {
+        throw "Hata Var";
+      }
+      else {
+        return value;
+      }
+
+    }), retryWhen(err => err.pipe(tap(x => console.log(x))))).subscribe(data => {
       console.log(data);
-    });
+    }, err => { console.log(err) });
+
+
 
   }
 
