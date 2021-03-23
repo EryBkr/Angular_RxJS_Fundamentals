4ad5fe92830b3368f88fb71b1f4eeaa629ca8ae7
Eray Bakır <info@eraybakir.com>
2021-03-23 16:53:12 +0300

    Subject Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { observable, Observable, of, Subject } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,36 @@ import { of } from "rxjs"; //RxJS operators lerini kullanabilmek için import et
 export class AppComponent {
 
   constructor() {
+
+
+    const myObservable = Observable.create(observer => {
+      observer.next(Math.random())
+    });
+
+    //Her iki datanın da farklı değerleri olacak.Her ikisinin de aynı data yı almasını istiyorsak Subject kullanmamız gerekiyor
+    myObservable.subscribe(data=>{
+      console.log("1. "+data);
+    });
+
+    myObservable.subscribe(data=>{
+      console.log("2. "+data);
+    });
+
+
+
+    //Subject yapısında subscribe olduktan sonra next metodu çağrılır.Her ikie data da aynı değere sahiptir..
+    const mySubject = new Subject();
+
+    mySubject.subscribe(data=>{
+      console.log("1. "+data);
+    });
+
+    mySubject.subscribe(data=>{
+      console.log("2. "+data);
+    });
+
+    mySubject.next(Math.random());
+
 
   }
 
