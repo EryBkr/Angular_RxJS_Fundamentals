865df00aab114e121705011b6131aff16c66f9d3
Eray Bakır <info@eraybakir.com>
2021-03-23 17:16:13 +0300

    AsyncSubject Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { ValueTransformer } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-import { observable, Observable, of, Subject } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
+import { AsyncSubject } from "rxjs"; //RxJS operators lerini kullanabilmek için import ettik
 
 @Component({
   selector: 'app-root',
@@ -11,24 +11,8 @@ export class AppComponent {
 
   constructor() {
 
-
-    const myObservable = Observable.create(observer => {
-      observer.next(Math.random())
-    });
-
-    //Her iki datanın da farklı değerleri olacak.Her ikisinin de aynı data yı almasını istiyorsak Subject kullanmamız gerekiyor
-    myObservable.subscribe(data=>{
-      console.log("1. "+data);
-    });
-
-    myObservable.subscribe(data=>{
-      console.log("2. "+data);
-    });
-
-
-
-    //Subject yapısında subscribe olduktan sonra next metodu çağrılır.Her ikie data da aynı değere sahiptir..
-    const mySubject = new Subject();
+    //Generic olarakta kullanılabilir
+    const mySubject = new AsyncSubject();
 
     mySubject.subscribe(data=>{
       console.log("1. "+data);
@@ -37,9 +21,12 @@ export class AppComponent {
     mySubject.subscribe(data=>{
       console.log("2. "+data);
     });
+
+
 
     mySubject.next(Math.random());
-
+    //Dataların alınabilmesi için Complete metodunun çalışması gerekiyor
+    mySubject.complete();
 
   }
 
